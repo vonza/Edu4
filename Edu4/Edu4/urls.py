2db3a0a7d36ac6bcc6dfbdc5f42e7e30c818747e
Nicholas Vonza <vonzanicholas@gmail.com>
2018-09-10 13:56:54 +0300

    env settings

---
+++ Edu4/Edu4/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Edu4.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Edu4.views.home, name='home'),
+    # url(r'^Edu4/', include('Edu4.Edu4.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
